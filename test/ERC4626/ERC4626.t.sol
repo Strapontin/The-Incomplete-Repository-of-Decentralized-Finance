e7bb91e503438335a357bbee562a0a70676ce9b6
Strapontin <antodu73@hotmail.fr>
2025-03-19 18:43:00 +0100

    Finished vault tests

--- test/ERC4626/ERC4626.t.sol
+++ test/ERC4626/ERC4626.t.sol
@@ -103,14 +103,65 @@ contract ERC4626Test is Test {
     }
 
     function testMintAndRedeemShares() public {
-        // Show with 2 users.
+        // Alice and Bob mint 100 shares and this transferes 100 tokens
+        vm.prank(alice);
+        assertEq(vault.mintShares(100), 100);
+        vm.prank(bob);
+        assertEq(vault.mintShares(100), 100);
+
+        // This simulates a reward of 100 tokens in the vault
+        vm.prank(helper);
+        token.mint(address(vault), 100);
+
+        // By redeeming 100 shares, they now get 150 tokens
+        vm.prank(alice);
+        assertEq(vault.redeem(100), 150);
+        assertEq(token.balanceOf(alice), 150);
     }
 
+    // User depositing after a rewards will have less share
     function testUserDepositingAfterRewardsCantClaimThemAndGetsLessAmountOfShares() public {
-        // Show that interacting with the vault after reward does change the shares given
+        vm.prank(alice);
+        assertEq(vault.deposit(100), 100);
+
+        // This simulates a reward of 100 tokens in the vault
+        vm.prank(helper);
+        token.mint(address(vault), 100);
+
+        // Because only Alice had shares, she's the only one to earn this bonus.
+        assertEq(vault.convertToAssets(vault.balanceOf(alice)), 200);
+
+        // Now, bob deposits 100 tokens in the vault, but gets only 50 shares
+        vm.prank(bob);
+        assertEq(vault.deposit(100), 50);
+
+        // Alice withdraws 200 tokens with her 100 shares
+        vm.prank(alice);
+        assertEq(vault.redeem(100), 200);
     }
 
     function testVaultCanLooseTokensAndUsersWillHaveLessAssets() public {
-        // Show the loss of rewards
+        // Alice and Bob deposits 100 tokens and get 100 shares
+        vm.prank(alice);
+        assertEq(vault.deposit(100), 100);
+        vm.prank(bob);
+        assertEq(vault.deposit(100), 100);
+
+        // This simulates a LOSS of 100 tokens in the vault
+        vm.prank(helper);
+        token.burn(address(vault), 100);
+
+        // Alice still has 100 shares, but now when she withdraws she will get 50 tokens
+        assertEq(vault.balanceOf(alice), 100);
+        assertEq(vault.convertToAssets(100), 50);
+
+        vm.prank(alice);
+        assertEq(vault.withdraw(50), 100);
+        assertEq(token.balanceOf(alice), 50);
+
+        // When Bob withdraws, he also gets 50 tokens
+        vm.prank(bob);
+        assertEq(vault.withdraw(50), 100);
+        assertEq(token.balanceOf(bob), 50);
     }
 }
